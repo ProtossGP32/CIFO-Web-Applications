6388d95a74404c153b6b6631a1ff71dc3ae62027
ProtossGP32 <19353116+ProtossGP32@users.noreply.github.com>
2022-12-19 22:38:54 +0100

    NEW: Software installation scripts for debian-based distros - Launch main `software_install.sh` as `sudo`

---
+++ scripts/software_installation/retrieve_latest_release.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# GitHub latest release package retrieval
+import json
+import requests
+from argparse import ArgumentParser
+
+def parse_arguments():
+    parser = ArgumentParser(
+        prog="retrieve_latest_release",
+        description="Retrieves latest release from a GitHub repository",
+        epilog="GitHub user and repository name are mandatory, as well as the file pattern (.deb, .tar.gz, etc...)"
+    )
+
+    parser.add_argument('--gh-user', dest='gh_user', type=str, help='The GitHub username', required=True)
+    parser.add_argument('--gh-repo', dest='gh_repo', help='The GitHub repository', required=True)
+    parser.add_argument('--pattern', dest='pattern', help='The pattern of the asst to download', default='.deb', required=False)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    print(f"Requesting latest release from {args.gh_user}/{args.gh_repo}, ({args.pattern} format)")
+    gh_api_url=f"https://api.github.com/repos/{args.gh_user}/{args.gh_repo}/releases/latest"
+    r = requests.get(gh_api_url)
+    response_text = json.loads(r.content)
+    #print(json.dumps(response_text, indent=2))
+
+    # Search fot the first asset that matches the file pattern
+    if "assets" in response_text:
+        for asset in response_text["assets"]:
+            if "name" in asset and asset["name"].endswith(args.pattern):
+                print(f'Downloading {asset["browser_download_url"]}...')
+                filename = asset["browser_download_url"].split('/')[-1]
+                download = requests.get(asset["browser_download_url"])
+                print(f"Saving content into {filename}")
+                open(filename, "wb").write(download.content)
+                break
+
+if __name__ == '__main__':
+    # Execute the main program
+    main()
