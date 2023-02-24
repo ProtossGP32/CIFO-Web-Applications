ecf96cca398f4bbc56d210b271020a79aa5f35e9
ProtossGP32 <19353116+ProtossGP32@users.noreply.github.com>
2023-02-24 12:12:46 +0100

    NEW: WIP universal planes spring boot exercise

---
+++ java-exercises/spring-boot-projects/cifojava2022-6-projects/universalplanes/src/main/java/org/cifojava20226/universalplanes/UniversalplanesApplication.java
@@ -0,0 +1,13 @@
+package org.cifojava20226.universalplanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalplanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalplanesApplication.class, args);
+	}
+
+}
---
+++ java-exercises/spring-boot-projects/cifojava2022-6-projects/universalplanes/src/test/java/org/cifojava20226/universalplanes/UniversalplanesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cifojava20226.universalplanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalplanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
