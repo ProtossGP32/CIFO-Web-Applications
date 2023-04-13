a3e439ae2ae56f758e5b0d03e98ae59985012b5e
ProtossGP32 <19353116+ProtossGP32@users.noreply.github.com>
2023-04-13 21:59:14 +0200

    chores: removed unused imports in BookImage

--- java-exercises/spring-boot-projects/spring-boot-lab-0/approach_1/src/main/java/com/springbootlab0/approach_1/bookImageMongoDB/BookImage.java
+++ java-exercises/spring-boot-projects/spring-boot-lab-0/approach_1/src/main/java/com/springbootlab0/approach_1/bookImageMongoDB/BookImage.java
@@ -1,10 +1,6 @@
 package com.springbootlab0.approach_1.bookImageMongoDB;
 
-import com.springbootlab0.approach_1.domain.Publication;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.*;
 import org.bson.types.Binary;
 import org.springframework.data.mongodb.core.mapping.Document;
